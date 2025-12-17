869038c4f681e5a5fbc0cbe381dae1277bb8b82d
Lakshya Gupta <lakshya@skyfall.ai>
2025-12-17 17:20:17 -0500

    Use job 1

--- main.py
+++ main.py
@@ -44,14 +44,14 @@ benchmark = "workarena_l3_agent_curriculum_eval"
 # Set reproducibility_mode = True for reproducibility
 # this will "ask" agents to be deterministic. Also, it will prevent you from launching if you have
 # local changes. For your custom agents you need to implement set_reproducibility_mode
-reproducibility_mode = True
+reproducibility_mode = False
 
 # Set relaunch = True to relaunch an existing study, this will continue incomplete
 # experiments and relaunch errored experiments
 relaunch = False
 
 ## Number of parallel jobs
-n_jobs = 4  # Make sure to use 1 job when debugging in VSCode
+n_jobs = 1  # Make sure to use 1 job when debugging in VSCode
 # n_jobs = -1  # to use all available cores
 
 
@@ -72,7 +72,7 @@ if __name__ == "__main__":  # necessary for dask backend
         n_jobs=n_jobs,
         parallel_backend="ray",
         strict_reproducibility=reproducibility_mode,
-        n_relaunch=3,
+        n_relaunch=1,
     )
 
     if reproducibility_mode:
